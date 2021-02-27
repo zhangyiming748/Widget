de7ec876be7957e4b36ccd622ebd00efd8c44705
Zen <zhangyiming748@gmail.com>
2021-02-27 18:58:46 +0800

    基本实现协程时可辨识信息输出

--- youtube-dl/cmd/ytd.go
+++ youtube-dl/cmd/ytd.go
@@ -23,3 +23,29 @@ func RunCommand(url string, wg *sync.WaitGroup, i int) {
 	fmt.Printf("下载NO.%d完成\n", i)
 	wg.Done()
 }
+func RunCmd(url string, wg *sync.WaitGroup, i int) {
+	cmd := exec.Command("youtube-dl", "--proxy", "127.0.0.1:8889", "-o", "/Users/zen/Downloads/trans/%(title)s.%(ext)s", "-f", "best", url)
+	// 命令的错误输出和标准输出都连接到同一个管道
+	stdout, err := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+	if err != nil {
+		fmt.Printf("cmd.StdoutPipe产生的错误:%v",err)
+	}
+	if err = cmd.Start(); err != nil {
+		fmt.Printf("cmd.Run产生的错误:%v",err)
+	}
+	// 从管道中实时获取输出并打印到终端
+	for {
+		tmp := make([]byte, 1024)
+		_, err := stdout.Read(tmp)
+		fmt.Printf("NO.%d的Goroutine输出:%s\n",i,string(tmp))
+		if err != nil {
+			break
+		}
+	}
+	fmt.Printf("下载NO.%d完成\n", i)
+	if err = cmd.Wait(); err != nil {
+		fmt.Println(err)
+	}
+	wg.Done()
+}
