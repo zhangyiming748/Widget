6771468fd6727f621ca422fb68f1e9b10d114dbc
Zen <zhangyiming748@gmail.com>
2021-02-28 20:04:57 +0800

    探测操作系统

--- youtube-dl/util/detectOS.go
+++ youtube-dl/util/detectOS.go
@@ -1,3 +1,32 @@
 package util
 
+import (
+
+	"runtime"
+)
+
 //探测当前操作系统
+
+func DetectOS() string{
+	arch := runtime.GOARCH
+
+	goos := runtime.GOOS
+
+	if arch == "arm" || goos == "linux" {
+		//树莓派
+		return "Raspbian"
+	}
+	if arch == "amd64" || goos == "darwin" {
+		//苹果
+		return "macOS"
+	}
+	if arch == "amd64" || goos == "windows" {
+		//Windows
+		return "windows"
+	}
+	if arch == "amd64" || goos == "linux" {
+		//ubuntu
+		return "ubuntu"
+	}
+	return ""
+}
--- youtube-dl/util/detectOS_test.go
+++ youtube-dl/util/detectOS_test.go
@@ -1,20 +1,19 @@
 package util
 
 import (
-	"log"
-	"runtime"
 	"testing"
 )
 
 func TestDetectOS(t *testing.T) {
-	a := runtime.GOARCH
+	//a := runtime.GOARCH
 	// 返回当前的系统架构
-	b := runtime.GOOS //
+	//b := runtime.GOOS //
 	// 返回当前的操作系统
-	log.Println(a, b)
+	//log.Println(a, b)
 	//pi=arm linux
 	//MacOS=amd64 darwin
 	//Windows=amd64 windows
 	//WSL=amd64 linux
 	//ubuntu=amd64 linux
+	DetectOS()
 }
