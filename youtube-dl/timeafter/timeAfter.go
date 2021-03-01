47b63ea1be5f10b64ebfb6598d29b97ee5faea7a
Zen <zhangyiming748@gmail.com>
2021-03-01 11:50:27 +0800

    测试时间格式

--- youtube-dl/timeafter/timeAfter.go
+++ youtube-dl/timeafter/timeAfter.go
@@ -1,1 +1,14 @@
 package timeafter
+
+import (
+	"log"
+	"time"
+)
+
+func subTime(){
+	t1:=time.Now()
+	time.Sleep(5*time.Second)
+	t2:=time.Now()
+	sub:=t2.Sub(t1)
+	log.Printf("sub = %v\tsub`s type is %T",sub,sub)
+}
