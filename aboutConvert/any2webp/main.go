04a21b5f8984d79590c6dc9c759decf40fed8a2e
zen <zhangyiming748@gmail.com>
2021-04-01 18:43:05 +0800

    将最大连接数写在配置文件中

--- aboutConvert/any2webp/main.go
+++ aboutConvert/any2webp/main.go
@@ -4,21 +4,33 @@ import (
 	"any2webp/command"
 	"any2webp/util"
 	"fmt"
+	"log"
+	"strconv"
 	"sync"
 )
 
 /**
 WebP支援的像素最大數量是16383x16383
 */
+var (
+	MaxGoroutine = util.GetVal("goroutine", "num")
+)
+
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+		}
+	}()
 	files := util.GetFiles()
-	ch := make(chan struct{}, 3)
+	max, _ := strconv.Atoi(MaxGoroutine)
+	ch := make(chan struct{}, maxz)
 	fmt.Println(files)
 	var wg sync.WaitGroup
 	for i, f := range files {
 		ch <- struct{}{}
 		wg.Add(1)
-		go command.RunCmd(f, &wg, i, ch)
+		go command.ToWebp(f, &wg, i, ch)
 	}
 	wg.Wait()
 }
