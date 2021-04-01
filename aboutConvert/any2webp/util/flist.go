09aa695d99a664adc4bcfc96b02b5ff6ed932d0c
zen <zhangyiming748@gmail.com>
2021-04-01 21:16:39 +0800

    主函数改为只用做判断

--- aboutConvert/any2webp/util/flist.go
+++ aboutConvert/any2webp/util/flist.go
@@ -11,14 +11,28 @@ import (
 func GetFiles() (flist []string) {
 	dir := GetVal("src", "fp")
 	files, _ := ioutil.ReadDir(dir)
-	for _, f := range files {
-		//fmt.Println(f.Name())
-		if strings.HasSuffix(f.Name(), GetVal("format", "pattern")) {
-			if !strings.HasPrefix(f.Name(), ".") {
-				absPath := strings.Join([]string{dir, f.Name()}, "/")
-				flist = append(flist, absPath)
+	if GetVal("runmode","mode")=="toWebp"{
+		for _, f := range files {
+			//fmt.Println(f.Name())
+			if strings.HasSuffix(f.Name(), ".jpg") {
+				if !strings.HasPrefix(f.Name(), ".") {
+					absPath := strings.Join([]string{dir, f.Name()}, "/")
+					flist = append(flist, absPath)
+				}
 			}
 		}
 	}
+	if GetVal("runmode","mode")=="webpTo"{
+		for _, f := range files {
+			//fmt.Println(f.Name())
+			if strings.HasSuffix(f.Name(), ".webp") {
+				if !strings.HasPrefix(f.Name(), ".") {
+					absPath := strings.Join([]string{dir, f.Name()}, "/")
+					flist = append(flist, absPath)
+				}
+			}
+		}
+	}
+
 	return
 }
