b292c661d945dae1b0442fe628918928c1d3a4cb
zen <zhangyiming748@gmail.com>
2021-04-01 18:10:35 +0800

    过多协程会报错too many open files

---
+++ aboutConvert/any2webp/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"any2webp/command"
+	"any2webp/util"
+	"fmt"
+	"sync"
+)
+/**
+WebP支援的像素最大數量是16383x16383
+ */
+func main(){
+	files:=util.GetFiles()
+	fmt.Println(files)
+	var wg sync.WaitGroup
+	for i,f:=range files{
+		wg.Add(1)
+		go command.RunCmd(f,&wg,i)
+	}
+	wg.Wait()
+}
