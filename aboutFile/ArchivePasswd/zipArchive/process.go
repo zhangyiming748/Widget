3b2ae7071de58b74d2de1157b426102e9ced55cc
zen <zhangyiming748@gmail.com>
2021-04-03 23:25:28 +0800

    无法复用代码

--- aboutFile/ArchivePasswd/zipArchive/process.go
+++ aboutFile/ArchivePasswd/zipArchive/process.go
@@ -1,1 +1,39 @@
 package zipArchive
+
+import (
+	"github.com/yeka/zip"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func UnZip(file, passwd string) {
+	r, err := zip.OpenReader(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		if f.IsEncrypted() {
+			f.SetPassword(passwd)
+		}
+		r, err := f.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		buf, err := ioutil.ReadAll(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer r.Close()
+
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			os.MkdirAll(f.Name, os.ModePerm)
+		} else {
+			ioutil.WriteFile(file+"/"+f.Name, buf, os.ModePerm)
+		}
+	}
+}
