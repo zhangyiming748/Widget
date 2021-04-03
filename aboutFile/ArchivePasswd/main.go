b1c068d5a8364673c37834e22bac93fd5547fce0
zen <zhangyiming748@gmail.com>
2021-04-04 00:00:21 +0800

    可显示密码单文件可复用暴力破解rar

---
+++ aboutFile/ArchivePasswd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"ArchivePasswd/rarArchive"
+	"ArchivePasswd/readPasswd"
+	"fmt"
+	"os"
+)
+
+var (
+	passwds = "./passwordDictionary.txt"
+)
+
+func main() {
+
+	var file string
+	if len(os.Args) == 2 {
+		file = os.Args[1]
+		//fmt.Println("文件名",file)
+	} else {
+		fmt.Println("没有指定文件,程序退出")
+		os.Exit(0)
+	}
+	passwords := readPasswd.ReadPasswd2Slice(passwds)
+	for _,ps:=range passwords{
+		rarArchive.UnRar(file,ps)
+	}
+
+}
