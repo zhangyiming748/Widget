5eac6a9021ca205cbce28cc7f9aeeafb49f66a01
zen <zhangyiming748@gmail.com>
2021-04-05 14:37:53 +0800

    引号有可能引起命令执行

--- aboutConvert/rotateVideos/shellCmd/rotateCmd.go
+++ aboutConvert/rotateVideos/shellCmd/rotateCmd.go
@@ -39,7 +39,7 @@ func LeftTo(src, dst, file string) {
 func RightTo(src, dst, file string) {
 	in := strings.Join([]string{src, file}, "/")
 	out := strings.Join([]string{dst, file}, "/")
-	cmd := exec.Command("ffmpeg", "-i", "\""+in+"\"", "-vf", "transpose=2", "\""+out+"\"")
+	cmd := exec.Command("ffmpeg", "-i", in, "-vf", "transpose=2", out)
 	// 命令的错误输出和标准输出都连接到同一个管道
 	stdout, err := cmd.StdoutPipe()
 	cmd.Stderr = cmd.Stdout
