5dc92ebaa5183342a72789c6a183f6ceed9548f8
zen <zhangyiming748@gmail.com>
2021-04-19 02:11:45 +0800

    更换其他格式测试

--- aboutFile/getEXIF/getGEO/getGEO_test.go
+++ aboutFile/getEXIF/getGEO/getGEO_test.go
@@ -3,6 +3,6 @@ package getGEO
 import "testing"
 
 func TestEXIF2GEO(t *testing.T) {
-	ret := EXIF2GEO("/Users/zen/Github/Widget/aboutFile/getEXIF/IMGX.jpg")
+	ret := EXIF2GEO("/Users/zen/Github/Widget/aboutFile/getEXIF/IMG6.jpg")
 	t.Logf("ret = %s", ret)
 }
