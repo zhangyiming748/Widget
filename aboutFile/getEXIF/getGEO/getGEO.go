676e94f7b9266d19af4db2ad385e901a1aa8cd0c
zen <zhangyiming748@gmail.com>
2021-04-18 21:44:49 +0800

    从相片中获取exif的geo信息

---
+++ aboutFile/getEXIF/getGEO/getGEO.go
@@ -0,0 +1,47 @@
+package getGEO
+
+import (
+	"fmt"
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/tiff"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func EXIF2GEO(fp string)string {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("recover:%v", err)
+		}
+	}()
+
+	//fname := "IMG4.jpg"
+
+	f, _ := os.Open(fp)
+	x, _ := exif.Decode(f)
+
+	//fmt.Printf("x:%v", x)
+	Longitude, _ := x.Get(exif.GPSLongitude)
+	Latitude, _ := x.Get(exif.GPSLatitude)
+	long := convert(Longitude)
+	lat := convert(Latitude)
+	ret:=fmt.Sprintf("%f,%v", long, lat)
+	return ret
+}
+func convert(tag *tiff.Tag) float64 {
+	step1 := fmt.Sprintf("%v", tag)
+	step2 := strings.Replace(step1, "\"", "", -1)
+	step3 := strings.Replace(step2, "[", "", -1)
+	step4 := strings.Replace(step3, "]", "", -1)
+	step5 := strings.Split(step4, ",")
+	hs := strings.Split(step5[0], "/")[0]
+	ms := strings.Split(step5[1], "/")[0]
+	ss := strings.Split(step5[2], "/")[0]
+	h, _ := strconv.Atoi(hs)
+	m, _ := strconv.Atoi(ms)
+	s, _ := strconv.Atoi(ss)
+	ret := float64(h) + float64(m)/60 + float64(s)/1000000/3600
+	return ret
+//116.36800384499999,39.91048431388889
+}
---
+++ aboutFile/getEXIF/getGEO/getGEO_test.go
@@ -0,0 +1,8 @@
+package getGEO
+
+import "testing"
+
+func TestEXIF2GEO(t *testing.T) {
+	ret:=EXIF2GEO("/Users/zen/Github/Widget/aboutFile/getEXIF/IMG4.jpg")
+	t.Logf("ret = %s",ret)
+}
