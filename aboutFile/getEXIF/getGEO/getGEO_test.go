98864fe26253de44ee9bdccc86cc25b9e5f2e82b
zen <zhangyiming748@gmail.com>
2021-04-19 00:55:20 +0800

    小数形式的图片

--- aboutFile/getEXIF/getGEO/getGEO_test.go
+++ aboutFile/getEXIF/getGEO/getGEO_test.go
@@ -3,6 +3,6 @@ package getGEO
 import "testing"
 
 func TestEXIF2GEO(t *testing.T) {
-	ret := EXIF2GEO("/Users/zen/Github/Widget/aboutFile/getEXIF/IMG4.jpg")
+	ret := EXIF2GEO("/Users/zen/Github/Widget/aboutFile/getEXIF/IMGX.jpg")
 	t.Logf("ret = %s", ret)
 }
