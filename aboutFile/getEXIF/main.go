72e2f1afd5fce5026d59c6f5cbf27fea49e3bdf7
zen <zhangyiming748@gmail.com>
2021-04-18 22:39:01 +0800

    改为从参数中传值

--- aboutFile/getEXIF/main.go
+++ aboutFile/getEXIF/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"getEXIF/decodeGEO"
 	"getEXIF/getGEO"
+	"os"
 )
 
 func main() {
-	fp := "IMG4.jpg"
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	if len(os.Args) < 2 {
+		panic(errors.New("第二个参数需要指定图片路径"))
+	} else {
+		fmt.Println(os.Args[0], os.Args[1])
+	}
+	fp := os.Args[1]
 	location := getGEO.EXIF2GEO(fp)
 	decodeGEO.Decode(location)
 	area := decodeGEO.DeXML("result.xml")
-	fmt.Printf("大致位置是:%s", area)
+	fmt.Printf("大致位置是:%s\n", area)
 }
