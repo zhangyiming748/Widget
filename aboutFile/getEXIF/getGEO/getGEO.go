317b7543e2b19139661395cd41bb3e143d313099
zen <zhangyiming748@gmail.com>
2021-04-19 02:11:28 +0800

    判断取值范围,符合条件直接查询

--- aboutFile/getEXIF/getGEO/getGEO.go
+++ aboutFile/getEXIF/getGEO/getGEO.go
@@ -26,11 +26,10 @@ func EXIF2GEO(fp string) string {
 	Long, err1 := strconv.ParseFloat(fmt.Sprintf("%v", Longitude), 64)
 	Lat, err2 := strconv.ParseFloat(fmt.Sprintf("%v", Latitude), 64)
 	if err1 == nil || err2 == nil {
-		if Long<180 ||Lat<90{
+		if Long < 180 || Lat < 90 {
 			ret := fmt.Sprintf("%f,%v", Long, Lat)
 			return ret
 		}
-
 	}
 	long := convert(Longitude)
 	lat := convert(Latitude)
@@ -46,10 +45,13 @@ func convert(tag *tiff.Tag) float64 {
 	hs := strings.Split(step5[0], "/")[0]
 	ms := strings.Split(step5[1], "/")[0]
 	ss := strings.Split(step5[2], "/")[0]
-	h, _ := strconv.ParseFloat(hs,64)
-	m, _ := strconv.ParseFloat(ms,64)
-	s, _ := strconv.ParseFloat(ss,64)
-	
-	ret := h + m/60 +s/1000000/3600
+	h, _ := strconv.ParseFloat(hs, 64)
+	m, _ := strconv.ParseFloat(ms, 64)
+	s, _ := strconv.ParseFloat(ss, 64)
+	if strings.Split(step5[0], "/")[1] == "1000" || strings.Split(step5[1], "/")[1] == "1000" || strings.Split(step5[2], "/")[1] == "1000" {
+		ret := h/1000 + m/1000/60 + s/1000/3600
+		return ret
+	}
+	ret := h + m/60 + s/1000000/3600
 	return ret
 }
