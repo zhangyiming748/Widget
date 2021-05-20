fadc3b76c6aa95f8efaf2a0c3354c90c6e55c243
zen <zhangyiming748@gmail.com>
2021-05-20 14:44:33 +0800

    练习解析JSON

---
+++ aboutFile/parseJSON/example.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
+)
+
+
+func main() {
+	filename:="static.json"
+	data, err := ioutil.ReadFile(filename)
+	if err !=nil{
+		fmt.Println(err)
+	}
+	json:=string(data)
+	value := gjson.Get(json, "retData.list搞")
+	println(value.String())
+
+}
