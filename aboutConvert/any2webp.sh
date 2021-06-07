5e72ad54d210ebe28e3a5f85b092b5b4c6e2f7f6
zen <zhangyiming748@gmail.com>
2021-06-07 21:51:01 +0800

    删除单文件

--- aboutConvert/any2webp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# webp最大宽高不得超过16383像素
-for x in ls *.jpg
-	do
-		cwebp $x -o ${x%.jpg}.webp
-	done
--- aboutConvert/any2webp/util/flist_test.go
+++
@@ -1,8 +0,0 @@
-package util
-
-import "testing"
-
-func TestFlist(t *testing.T) {
-	ret := GetFiles()
-	t.Logf("ret is %v", ret)
-}
