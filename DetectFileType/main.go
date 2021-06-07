64c5e9d52f1f0f38eedd1b25dc1dbf7da62efb98
zen <zhangyiming748@gmail.com>
2021-06-07 21:49:34 +0800

    探测文本文件编码

--- DetectFileType/main.go
+++ DetectFileType/main.go
@@ -5,18 +5,19 @@ import (
 	"fmt"
 	"os"
 )
+
 //探测文件类型,需要放到被探测文件同级目录
 func main() {
-	 //for i,v:=range os.Args{
-	 //	fmt.Printf("第%d个参数是%s\n",i+1,v)
-	 //}
-	 if len(os.Args)<=1{
-	 	fmt.Println("需要输入文件名作为参数")
-	 	os.Exit(1)
-	 }
-	 for i:=1;i<len(os.Args);i++{
-	 	t:=getFileType.Master(os.Args[i])
-	 	fmt.Printf("第 %v 个文件 %v 的文件类型是 %s\n",i,os.Args[i],t)
-	 }
-	 //getFileType.Master(os.Args[1])
+	//for i,v:=range os.Args{
+	//	fmt.Printf("第%d个参数是%s\n",i+1,v)
+	//}
+	if len(os.Args) <= 1 {
+		fmt.Println("需要输入文件名作为参数")
+		os.Exit(1)
+	}
+	for i := 1; i < len(os.Args); i++ {
+		t := getFileType.Master(os.Args[i])
+		fmt.Printf("第 %v 个文件 %v 的文件类型是 %s\n", i, os.Args[i], t)
+	}
+	//getFileType.Master(os.Args[1])
 }
