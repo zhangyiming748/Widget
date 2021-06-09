b91e3651b50a88a3daca5e94c34f1ba28a4447cf
zen <zhangyiming748@gmail.com>
2021-06-09 21:17:52 +0800

    添加批量格式化功能

--- allTools/init.sh
+++ allTools/init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 find . -type f -name "*.log" -exec rm {} \;
+find . -name "*.go" -exec gofmt -w {} \;
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o forLinux main.go
 CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o forRaspi main.go
 CGO_ENABLED=1 GOOS=windows GOARCH=386 go build -o forWin32.exe main.go
 CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o forWin64.exe main.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o forMac main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm go build -o forM1 main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o forM1 main.go
