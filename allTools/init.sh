0bb1aa342a9a5995a72c8a6d9e170527abfb91c5
zen <zhangyiming748@gmail.com>
2021-06-10 19:15:58 +0800

    静态编译

--- allTools/init.sh
+++ allTools/init.sh
@@ -8,7 +8,7 @@ find . -name ".DS_Store" -exec rm {} \;
 echo 编译二进制文件
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o forLinux main.go
 CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o forRaspi main.go
-CGO_ENABLED=1 GOOS=windows GOARCH=386 go build -o forWin32.exe main.go
-CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o forWin64.exe main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o forWin32.exe main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o forWin64.exe main.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o forMac main.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o forM1 main.go
