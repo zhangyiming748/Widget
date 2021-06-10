bc693d175e92af167d2ba360f18cf54ac2ed3720
zen <zhangyiming748@gmail.com>
2021-06-10 17:47:25 +0800

    合并shell

--- aboutConvert/flv2mp4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in /home/pi/Share/ffmpeg/flv/*
-	do
-		echo $file
-		ffmpeg -i $file $file".mp4"
--- aboutConvert/mergeVideo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ffmpeg -i "concat:0.ts|1.ts|2.ts|3.ts|4.ts|5.ts|6.ts|7.ts|8.ts|9.ts|10.ts|11.ts|12.ts|13.ts|14.ts" -c copy "Shemale Jonelle Brooks fucks asian ladyboy babe Kartoon.mp4"
