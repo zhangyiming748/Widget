821318c9f347c82af1f9699188d0adb15a680895
zen <zhangyiming748@gmail.com>
2021-06-11 17:20:00 +0800

    测试第三方exec包使用方法

--- allTools/testUnit/All_test.go
+++ allTools/testUnit/All_test.go
@@ -2,10 +2,47 @@ package testUnit
 
 import (
 	"fmt"
+	"github.com/go-cmd/cmd"
 	"testing"
 	"time"
 )
 
+func TestGo_cmd(t *testing.T) {
+	// Start a long-running process, capture stdout and stderr
+	findCmd := cmd.NewCmd("find", "/", "--name", "needle")
+	statusChan := findCmd.Start() // non-blocking
+
+	ticker := time.NewTicker(2 * time.Second)
+
+	// Print last line of stdout every 2s
+	go func() {
+		for range ticker.C {
+			status := findCmd.Status()
+			n := len(status.Stdout)
+			fmt.Println(status.Stdout[n-1])
+		}
+	}()
+
+	// Stop command after 1 hour
+	go func() {
+		<-time.After(1 * time.Hour)
+		findCmd.Stop()
+	}()
+
+	// Check if command is done
+	select {
+	case finalStatus := <-statusChan:
+		// done
+		t.Logf("finish%v", finalStatus)
+	default:
+		// no, still running
+	}
+
+	// Block waiting for command to exit, be stopped, or be killed
+	finalStatus := <-statusChan
+	t.Logf("finish%v", finalStatus)
+
+}
 func TestPrint(t *testing.T) {
 	printInLine()
 }
