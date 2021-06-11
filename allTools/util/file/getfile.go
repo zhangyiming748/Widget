cc0d41c63a668ee327e8eac5c774420684af7f44
zen <zhangyiming748@gmail.com>
2021-06-11 17:20:26 +0800

    获取目录下所有文件

--- allTools/util/file/getfile.go
+++ allTools/util/file/getfile.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"allTools/util/log"
 	"io/ioutil"
 	"strings"
 )
@@ -13,13 +12,30 @@ func GetFiles(dir, pattern string) []string {
 	for _, f := range files {
 		//fmt.Println(f.Name())
 		if l := strings.Split(f.Name(), ".")[0]; len(l) != 0 {
-			log.Info.Printf("有效的文件:%v\n", f.Name())
+			//log.Debug.Printf("有效的文件:%v\n", f.Name())
 			if strings.HasSuffix(f.Name(), pattern) {
-				log.Info.Printf("有效的目标文件:%v\n", f.Name())
+				//log.Debug.Printf("有效的目标文件:%v\n", f.Name())
 				//absPath := strings.Join([]string{dir, f.Name()}, "/")
 				//log.Info.Printf("目标文件的绝对路径:%v\n", absPath)
 				aim = append(aim, f.Name())
 			}
+		}
+	}
+	return aim
+}
+func GetAllFiles(dir string) []string {
+	files, _ := ioutil.ReadDir(dir)
+	aim := []string{}
+	for _, f := range files {
+		//fmt.Println(f.Name())
+		if l := strings.Split(f.Name(), ".")[0]; len(l) != 0 {
+			//log.Debug.Printf("有效的文件:%v\n", f.Name())
+			//if strings.HasSuffix(f.Name(), pattern) {
+			//	log.Debug.Printf("有效的目标文件:%v\n", f.Name())
+			//	//absPath := strings.Join([]string{dir, f.Name()}, "/")
+			//	//log.Info.Printf("目标文件的绝对路径:%v\n", absPath)
+			aim = append(aim, f.Name())
+			//}
 		}
 	}
 	return aim
