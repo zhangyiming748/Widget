efad68fce554992d2a17fe9b536704e76e4d83ed
zen <zhangyiming748@gmail.com>
2022-05-20 21:29:08 +0800

    使用结构体接收返回值

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
+	github.com/gookit/goutil v0.5.2
 	github.com/k0kubun/go-ansi v0.0.0-20180517002512-3bf9e2903213
 	github.com/schollz/progressbar/v3 v3.8.6
 	github.com/widuu/goini v0.0.0-20180603013956-56a38bd2e09b
@@ -11,10 +12,12 @@ require (
 )
 
 require (
+	github.com/gookit/color v1.5.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
 	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
--- weather/weather.go
+++ weather/weather.go
@@ -3,6 +3,7 @@ package weather
 import (
 	"Widget/util/log"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os/exec"
@@ -12,6 +13,41 @@ import (
 var (
 	key = "3e469a45a72b31c120b0baf6609ae877"
 )
+
+type location struct {
+	Status    string `json:"status"`
+	Count     string `json:"count"`
+	Info      string `json:"info"`
+	Infocode  string `json:"infocode"`
+	Forecasts []struct {
+		City       string `json:"city"`
+		Adcode     string `json:"adcode"`
+		Province   string `json:"province"`
+		Reporttime string `json:"reporttime"`
+		Casts      []struct {
+			Date         string `json:"date"`
+			Week         string `json:"week"`
+			Dayweather   string `json:"dayweather"`
+			Nightweather string `json:"nightweather"`
+			Daytemp      string `json:"daytemp"`
+			Nighttemp    string `json:"nighttemp"`
+			Daywind      string `json:"daywind"`
+			Nightwind    string `json:"nightwind"`
+			Daypower     string `json:"daypower"`
+			Nightpower   string `json:"nightpower"`
+		} `json:"casts"`
+	} `json:"forecasts"`
+}
+
+var week = map[string]string{
+	"1": "星期一",
+	"2": "星期二",
+	"3": "星期三",
+	"4": "星期四",
+	"5": "星期五",
+	"6": "星期六",
+	"7": "星期日",
+}
 
 const (
 	北京市石景山区     = "110107"
@@ -31,123 +67,35 @@ func Weather() {
 	}
 }
 func format(adcode string) {
-	var m map[string]interface{}
+	//var m map[string]interface{}
+	var l location
 	url := "https://restapi.amap.com/v3/weather/weatherInfo?output=json&extensions=all&city=" + adcode + "&key=" + key
 	response, _ := http.Get(url)
 	defer response.Body.Close() //在回复后必须关闭回复的主体
 	body, _ := ioutil.ReadAll(response.Body)
-	_ = json.Unmarshal(body, &m)
-	reporttime := m["forecasts"].([]interface{})[0].(map[string]interface{})["reporttime"]
-	log.Info.Printf("%v发布\n", reporttime)
-	today_date := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["date"]
-	today_week := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["week"]
-	switch today_week {
-	case "1":
-		today_week = "一"
-	case "2":
-		today_week = "二"
-	case "3":
-		today_week = "三"
-	case "4":
-		today_week = "四"
-	case "5":
-		today_week = "五"
-	case "6":
-		today_week = "六"
-	case "7":
-		today_week = "日"
-	}
-	today_dayweather := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["dayweather"]
-	today_nightweather := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["nightweather"]
-	today_daytemp := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["daytemp"]
-	today_nighttemp := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["nighttemp"]
-	today_daywind := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["daywind"]
-	today_nightwind := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["nightwind"]
-	today_daypower := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["daypower"]
-	today_nightpower := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[0].(map[string]interface{})["nightpower"]
-	log.Info.Printf("%v 星期%v\t白天%v最高温度%v\u00B0C %v风%v级\t夜间%v最低温度%v\u00B0C %v风%v级\n", today_date, today_week, today_dayweather, today_daytemp, today_daywind, today_daypower, today_nightweather, today_nighttemp, today_nightwind, today_nightpower)
-	after_24_date := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["date"]
-	after_24_week := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["week"]
-	switch after_24_week {
-	case "1":
-		after_24_week = "一"
-	case "2":
-		after_24_week = "二"
-	case "3":
-		after_24_week = "三"
-	case "4":
-		after_24_week = "四"
-	case "5":
-		after_24_week = "五"
-	case "6":
-		after_24_week = "六"
-	case "7":
-		after_24_week = "日"
-	}
-	after_24_dayweather := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["dayweather"]
-	after_24_nightweather := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["nightweather"]
-	after_24_daytemp := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["daytemp"]
-	after_24_nighttemp := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["nighttemp"]
-	after_24_daywind := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["daywind"]
-	after_24_nightwind := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["nightwind"]
-	after_24_daypower := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["daypower"]
-	after_24_nightpower := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[1].(map[string]interface{})["nightpower"]
-	log.Info.Printf("%v 星期%v\t白天%v最高温度%v\u00B0C %v风%v级\t夜间%v最低温度%v\u00B0C %v风%v级\n", after_24_date, after_24_week, after_24_dayweather, after_24_daytemp, after_24_daywind, after_24_daypower, after_24_nightweather, after_24_nighttemp, after_24_nightwind, after_24_nightpower)
-	after_48_date := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["date"]
-	after_48_week := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["week"]
-	switch after_48_week {
-	case "1":
-		after_48_week = "一"
-	case "2":
-		after_48_week = "二"
-	case "3":
-		after_48_week = "三"
-	case "4":
-		after_48_week = "四"
-	case "5":
-		after_48_week = "五"
-	case "6":
-		after_48_week = "六"
-	case "7":
-		after_48_week = "日"
-	}
-	after_48_dayweather := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["dayweather"]
-	after_48_nightweather := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["nightweather"]
-	after_48_daytemp := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["daytemp"]
-	after_48_nighttemp := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["nighttemp"]
-	after_48_daywind := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["daywind"]
-	after_48_nightwind := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["nightwind"]
-	after_48_daypower := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["daypower"]
-	after_48_nightpower := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[2].(map[string]interface{})["nightpower"]
-	log.Info.Printf("%v 星期%v\t白天%v最高温度%v\u00B0C %v风%v级\t夜间%v最低温度%v\u00B0C %v风%v级\n", after_48_date, after_48_week, after_48_dayweather, after_48_daytemp, after_48_daywind, after_48_daypower, after_48_nightweather, after_48_nighttemp, after_48_nightwind, after_48_nightpower)
 
-	after_72_date := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["date"]
-	after_72_week := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["week"]
-	switch after_72_week {
-	case "1":
-		after_72_week = "一"
-	case "2":
-		after_72_week = "二"
-	case "3":
-		after_72_week = "三"
-	case "4":
-		after_72_week = "四"
-	case "5":
-		after_72_week = "五"
-	case "6":
-		after_72_week = "六"
-	case "7":
-		after_72_week = "日"
+	if err := json.Unmarshal(body, &l); err != nil {
+		log.Warn.Panicln("json解析出错")
 	}
-	after_72_dayweather := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["dayweather"]
-	after_72_nightweather := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["nightweather"]
-	after_72_daytemp := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["daytemp"]
-	after_72_nighttemp := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["nighttemp"]
-	after_72_daywind := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["daywind"]
-	after_72_nightwind := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["nightwind"]
-	after_72_daypower := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["daypower"]
-	after_72_nightpower := m["forecasts"].([]interface{})[0].(map[string]interface{})["casts"].([]interface{})[3].(map[string]interface{})["nightpower"]
-	log.Info.Printf("%v 星期%v\t白天%v最高温度%v\u00B0C %v风%v级\t夜间%v最低温度%v\u00B0C %v风%v级\n\n", after_72_date, after_72_week, after_72_dayweather, after_72_daytemp, after_72_daywind, after_72_daypower, after_72_nightweather, after_72_nighttemp, after_72_nightwind, after_72_nightpower)
+	//dump.P(l)
+	log.Info.Printf("%s发布预报\n", l.Forecasts[0].Reporttime)
+	for i, cast := range l.Forecasts[0].Casts {
+		var witchDay string
+		switch i {
+		case 0:
+			witchDay = "今天"
+		case 1:
+			witchDay = "明天"
+		case 2:
+			witchDay = "后天"
+		case 3:
+			witchDay = "大后天"
+		}
+		log.Info.Printf("%s%s\n", cast.Date, week[cast.Week])
+		log.Info.Printf("%s白天%s%s\u2103,%s风%s级\n", witchDay, cast.Dayweather, cast.Daytemp, cast.Daywind, cast.Daypower)
+		log.Info.Printf("%s夜晚%s%s\u2103,%s风%s级\n", witchDay, cast.Nightweather, cast.Nighttemp, cast.Nightwind, cast.Nightpower)
+	}
+	fmt.Println()
 }
 
 const (
